457c048d149208acfa1057ea236a83683766cdbd
hyeeunMokMobileWeb <52890203+hyeeunMokMobileWeb@users.noreply.github.com>
2019-07-24 05:16:26 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,31 @@
+import { SongInfo } from '../../app/songInfo';
+
+export const MYSONGS: SongInfo[] = [
+  {
+    songName: 'La traviata',
+    artist: 'Giuseppe Verdi',
+    genre: 'Opera',
+    yearReleased: 1981,
+    SongPicture: 'assets/images/verdi.jpg'
+  },
+  {
+    songName: 'The Book of Mormon',
+    artist: 'Trey Parker',
+    genre: 'Musical comedy',
+    yearReleased: 2011,
+    SongPicture: 'assets/images/mormon.jpg'
+  },
+  {
+    songName: 'I was Born to Love You',
+    artist: 'Queen',
+    genre: 'Pop rock',
+    yearReleased: 1985,
+    SongPicture: 'assets/images/queen.jpg'},
+  {
+    songName: 'Poker Face',
+    artist: 'Lady Gaga',
+    genre: 'Synth-pop, dance-pop',
+    yearReleased: 2008,
+    SongPicture: 'assets/images/ladygaga.png'
+  }
+];
